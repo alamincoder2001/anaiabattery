eba520570b32f58d79151d50cd5967b7220f6593
alamincoder2001 <114378678+alamincoder2001@users.noreply.github.com>
2023-02-19 16:26:00 +0600

    add all

---
+++ application/views/Administrator/edit/batch_edit.php
@@ -0,0 +1,151 @@
+<div class="row">
+<div class="col-xs-12">
+	<!-- PAGE CONTENT BEGINS -->
+	<div class="form-horizontal">
+		
+		<div class="form-group">
+			<label class="col-sm-3 control-label no-padding-right" for="form-field-1"> Batch Serial  </label>
+			<label class="col-sm-1 control-label no-padding-right">:</label>
+			<div class="col-sm-8">
+				<input type="text" id="batchname" name="batchname" placeholder="Batch Serial" value="<?php echo $selected->Batch_Serial; ?>" class="col-xs-10 col-sm-4" />
+				<input name="id" id="id" type="hidden" value="<?php echo $selected->id; ?>" />
+			</div>
+		</div>
+		
+		<div class="form-group">
+			<label class="col-sm-3 control-label no-padding-right" for="form-field-1"></label>
+			<label class="col-sm-1 control-label no-padding-right"></label>
+			<div class="col-sm-8">
+				    <button type="button" class="btn btn-sm btn-success" onclick="submit()" name="btnSubmit">
+						Update
+						<i class="ace-icon fa fa-arrow-right icon-on-right bigger-110"></i>
+					</button>
+			</div>
+		</div>
+		
+</div>
+</div>
+</div>
+
+
+			
+<div class="row">
+	<div class="col-xs-12">
+		<div class="clearfix">
+			<div class="pull-right tableTools-container"></div>
+		</div>
+		<div class="table-header">
+			Batch Information
+		</div>
+
+		<!-- div.table-responsive -->
+
+		<!-- div.dataTables_borderWrap -->
+		<div id="saveResult">
+			<table id="dynamic-table" class="table table-striped table-bordered table-hover">
+				<thead>
+					<tr>
+						<th class="center" style="display:none;">
+							<label class="pos-rel">
+								<input type="checkbox" class="ace" />
+								<span class="lbl"></span>
+							</label>
+						</th>
+						<th>SL No</th>
+						<th>Batch Serial</th>
+
+						<th>Action</th>
+						<th></th>
+						<th></th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<?php 
+					$query = $this->db->query("SELECT * FROM tbl_batch");
+					$row = $query->result();
+					 ?>
+					<?php $i=1; foreach($row as $row){ ?>
+					<tr>
+						<td class="center" style="display:none;">
+							<label class="pos-rel">
+								<input type="checkbox" class="ace" />
+								<span class="lbl"></span>
+							</label>
+						</td>
+
+						<td><?php echo $i++; ?></td>
+						<td><a href="#"><?php echo $row->Batch_Serial; ?></a></td>
+						<td>
+						<div class="hidden-sm hidden-xs action-buttons">
+								<a class="blue" href="#">
+									<i class="ace-icon fa fa-search-plus bigger-130"></i>
+								</a>
+
+								<a class="green" href="<?php echo base_url() ?>batchedit/<?php echo $row->id; ?>" title="Eidt" onclick="return confirm('Are you sure you want to Edit this item?');">
+									<i class="ace-icon fa fa-pencil bigger-130"></i>
+								</a>
+
+								<a class="red" href="#" onclick="deleted(<?php echo $row->id; ?>)">
+									<i class="ace-icon fa fa-trash-o bigger-130"></i>
+								</a>
+							</div>
+						</td>
+
+						<td class="hidden-480">
+							<span class="label label-sm label-info arrowed arrowed-righ"><?php //echo $row->ProductCategory_Name; ?></span>
+						</td>
+
+						<td></td>
+					</tr>
+					
+					<?php } ?>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</div>
+
+
+<script type="text/javascript">
+    function submit(){
+        var batchname= $("#batchname").val();
+        var id= $("#id").val();
+        if(batchname==""){
+            $("#batchname").css("border-color","red");
+            return false;
+        }
+        var inputdata = 'batchname='+batchname+'&id='+id;
+        var urldata = "<?php echo base_url(); ?>batchupdate";
+        $.ajax({
+            type: "POST",
+            url: urldata,
+            data: inputdata,
+            success:function(data){
+				if(data=="true"){
+					alert("Update Success");
+					location.href = "/batch";
+				}
+            }
+        });
+    }
+</script>
+<script type="text/javascript">
+    function deleted(id){
+        var deletedd= id;
+        var inputdata = 'deleted='+deletedd;
+		var confirmation = confirm("are you sure you want to delete this ?");
+        var urldata = "<?php echo base_url();?>Administrator/page/batchdelete";
+		if(confirmation){
+        $.ajax({
+            type: "POST",
+            url: urldata,
+            data: inputdata,
+            success:function(data){
+                alert("Delete Success");
+				location.reload();
+            }
+        });
+		};
+    }
+</script>	
---
+++ application/views/Administrator/sales/imei_return.php
@@ -0,0 +1,54 @@
+<div class="row" id="purchaseReturn">
+	<div class="col-xs-12 col-md-12 col-lg-12" style="border-bottom:1px #ccc solid;">
+		<div class="form-group" style="margin-top:10px;">
+			<label class="col-sm-1 col-sm-offset-1 control-label no-padding-right" for="purchaseInvoiceno"> Invoice no </label>
+			<div class="col-sm-2">
+                <select class="chosen-select" id="imei_number">
+                <option value="0" >Select a IMEI</option>
+                <?php $returning_imeis =  $this->db->query("SELECT ps_imei_number,ps_prod_id FROM  tbl_product_serial_numbers WHERE ps_brunch_id=? AND  ps_s_r_status='no' OR ps_s_r_status=null",$this->session->userdata('BRANCHid'))->result(); 
+                foreach($returning_imeis as $key=>$value){?>
+                    <option value="<?= $value->ps_imei_number?>"><?= $value->ps_imei_number?></option>
+                <?php } ?>
+                </select>
+ 			</div>
+             <div class="col-sm-2">
+             </div>
+		</div>
+
+        <span id="render_html">
+        </span>
+	</div>
+		</div>
+	</div>
+</div>
+<script src="<?php echo base_url();?>assets/js/vue/axios.min.js"></script>
+<script>
+     $(document).on('change','#imei_number',function(){
+        var imei_number = $(this).val();
+        $.ajax({
+            url:"<?= base_url()?>Administrator/Sales/get_return_imei_number",
+            method:"POST",
+            data:{imei_number:imei_number},
+            success:function(data){
+                $('#render_html').html(data);
+            }
+        })
+     })
+
+     $(document).on('click','#parchase_return_btn',function(){
+            let imei_number = $(this).attr('data-id');
+            let return_amout = $(this).attr('data-amount');
+            let prod_id = $(this).attr('data-prod_id');
+            
+            axios.post('/sale_imei_return',{imei_number:imei_number,return_amout:return_amout,prod_id:prod_id})
+            .then((res)=>{
+                if (res.data=='return') {
+                    alert('Return Success')
+                   window.location.reload()
+                }else{
+                    alert("Somthing Wrong")
+                    return false;
+                }
+            })
+     });
+</script>
